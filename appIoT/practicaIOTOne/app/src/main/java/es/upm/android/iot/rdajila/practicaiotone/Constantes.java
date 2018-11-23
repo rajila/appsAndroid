de32aff3e1481a0ffb6b33ee55e9380fb6c7c067
rajila <rajila@espol.edu.ec>
2018-11-23 23:28:21 +0100

    PracticaOne RGB LEDs Android

---
+++ appIoT/practicaIOTOne/app/src/main/java/es/upm/android/iot/rdajila/practicaiotone/Constantes.java
@@ -0,0 +1,8 @@
+package es.upm.android.iot.rdajila.practicaiotone;
+
+public final class Constantes
+{
+    public static final String PINRED = "BCM5";
+    public static final String PINGREEN = "BCM6";
+    public static final String PINBLUE = "BCM13";
+}
