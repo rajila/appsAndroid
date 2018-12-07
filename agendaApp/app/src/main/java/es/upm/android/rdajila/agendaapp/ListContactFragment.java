d6175a3fb0807d2a1e8bdb8cd1394cacccd6c959
Ronald <rdajila@gmail.com>
2018-12-07 18:05:26 +0100

    Add new Contact

--- agendaApp/app/src/main/java/es/upm/android/rdajila/agendaapp/ListContactFragment.java
+++ agendaApp/app/src/main/java/es/upm/android/rdajila/agendaapp/ListContactFragment.java
@@ -1,5 +1,6 @@
 package es.upm.android.rdajila.agendaapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -8,10 +9,12 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import es.upm.android.rdajila.agendaapp.contract.ContactContract;
 import es.upm.android.rdajila.agendaapp.crudcontact.AddEditContact;
@@ -29,6 +32,8 @@ import es.upm.android.rdajila.agendaapp.util.Constant;
  */
 public class ListContactFragment extends Fragment
 {
+    private static final String TAG = ListContactFragment.class.getSimpleName();
+
     private ScheduleDbHelper _db;
     private ListView _listContact;
     private ContactCursorAdapter _contactAdaptador;
@@ -43,8 +48,8 @@ public class ListContactFragment extends Fragment
         // Inflate the layout for this fragment
         View _viewLayout = inflater.inflate(R.layout.fragment_list_contact, container, false);
         _listContact = (ListView) _viewLayout.findViewById(R.id._listData);
-        //_contactAdaptador = new ContactCursorAdapter(getActivity(),null);
-        _contactAdaptador = new ContactCursorAdapter(getActivity(), _db.getAllContacts());
+        _contactAdaptador = new ContactCursorAdapter(getActivity(),null);
+        //_contactAdaptador = new ContactCursorAdapter(getActivity(), _db.getAllContacts());
         _listContact.setAdapter(_contactAdaptador);
 
         _btnAdd = (FloatingActionButton)_viewLayout.findViewById(R.id._btnAdd);
@@ -53,18 +58,41 @@ public class ListContactFragment extends Fragment
             public void onClick(View v) { actionAddContact(); }
         });
 
-        //loadContacts();
+        loadContacts();
 
         return _viewLayout;
     }
 
     private void actionAddContact()
     {
-        Intent _intentView = new Intent(getActivity(),AddEditContact.class);
-        startActivityForResult(_intentView, Constant._REQUEST_ADD_CONTACT);
+        Intent _intentView = new Intent( getActivity(), AddEditContact.class );
+        startActivityForResult( _intentView, Constant._REQUEST_ADD_CONTACT ); // Constant._REQUEST_ADD_CONTACT valor del resultCode
     }
 
-    /*private void loadContacts()
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (Activity.RESULT_OK == resultCode)
+        {
+            switch (requestCode)
+            {
+                case Constant._REQUEST_ADD_CONTACT:
+                    showSavedMessage();
+                    break;
+                //case REQUEST_UPDATE_DELETE_LAWYER:
+                //    loadLawyers();
+                //    break;
+            }
+            loadContacts();
+        }
+    }
+
+    private void showSavedMessage()
+    {
+        Toast.makeText(getActivity(), "Contacto guardado correctamente", Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadContacts()
     {
         new ContactLoadTask().execute();
     }
@@ -84,5 +112,5 @@ public class ListContactFragment extends Fragment
                 // Mostrar empty state
             }
         }
-    }*/
+    }
 }
--- agendaApp/app/src/main/java/es/upm/android/rdajila/agendaapp/crudcontact/AddEditContactFragment.java
+++ agendaApp/app/src/main/java/es/upm/android/rdajila/agendaapp/crudcontact/AddEditContactFragment.java
@@ -1,14 +1,24 @@
 package es.upm.android.rdajila.agendaapp.crudcontact;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import es.upm.android.rdajila.agendaapp.ListContactFragment;
 import es.upm.android.rdajila.agendaapp.R;
+import es.upm.android.rdajila.agendaapp.data.ScheduleDbHelper;
+import es.upm.android.rdajila.agendaapp.entity.Contact;
 import es.upm.android.rdajila.agendaapp.util.Constant;
 
 
@@ -22,7 +32,28 @@ import es.upm.android.rdajila.agendaapp.util.Constant;
  */
 public class AddEditContactFragment extends Fragment
 {
+    private ScheduleDbHelper _db;
+
+    private static final String TAG = AddEditContactFragment.class.getSimpleName();
+
     private String _idContact;
+
+    private Button _btnAceptar;
+
+    private TextInputLayout _tilName;
+    private EditText _fieldName;
+
+    private TextInputLayout _tilDirection;
+    private EditText _fieldDirection;
+
+    private TextInputLayout _tilMobile;
+    private EditText _fieldMobile;
+
+    private TextInputLayout _tilPhone;
+    private EditText _fieldPhone;
+
+    private TextInputLayout _tilEmail;
+    private EditText _fieldEmail;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -34,9 +65,81 @@ public class AddEditContactFragment extends Fragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        _db = new ScheduleDbHelper(getActivity());
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit_contact, container, false);
+        View _viewLayout = inflater.inflate(R.layout.fragment_add_edit_contact, container, false);
+
+        _btnAceptar = (Button) _viewLayout.findViewById(R.id.boton_aceptar);
+        _btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { saveContact(); }
+        });
+
+        _tilName = (TextInputLayout)_viewLayout.findViewById(R.id._til_name);
+        _fieldName = (EditText)_viewLayout.findViewById(R.id._field_name);
+
+        _tilDirection = (TextInputLayout)_viewLayout.findViewById(R.id._til_direction);
+        _fieldDirection = (EditText)_viewLayout.findViewById(R.id._field_direction);
+
+        _tilMobile = (TextInputLayout)_viewLayout.findViewById(R.id._til_mobile);
+        _fieldMobile = (EditText)_viewLayout.findViewById(R.id._field_mobile);
+
+        _tilPhone = (TextInputLayout)_viewLayout.findViewById(R.id._til_phone);
+        _fieldPhone = (EditText)_viewLayout.findViewById(R.id._field_phone);
+
+        _tilEmail = (TextInputLayout)_viewLayout.findViewById(R.id._til_email);
+        _fieldEmail = (EditText)_viewLayout.findViewById(R.id._field_email);
+
+        return _viewLayout;
+    }
+
+    private void saveContact()
+    {
+        Log.i(TAG, "Save Contact!!");
+
+        String _name = _fieldName.getText().toString();
+        String _direction = _fieldDirection.getText().toString();
+        String _mobile = _fieldMobile.getText().toString();
+        String _phone = _fieldPhone.getText().toString();
+        String _email = _fieldEmail.getText().toString();
+
+        Contact _contact = new Contact(_name,_direction,_mobile,_phone,_email);
+        new AddEditContactTask().execute(_contact);
+    }
+
+    private void showListContactScreen(Boolean requery)
+    {
+        if ( !requery )
+        {
+            //showAddEditError();
+            getActivity().setResult(Activity.RESULT_CANCELED);
+        } else getActivity().setResult(Activity.RESULT_OK);
+
+        getActivity().finish();
+    }
+
+    private class AddEditContactTask extends AsyncTask<Contact, Void, Boolean>
+    {
+        @Override
+        protected Boolean doInBackground(Contact... contacts) {
+            if (_idContact != null)
+            {
+                Log.i(TAG,"Update Contact");
+                //return mLawyersDbHelper.updateLawyer(lawyers[0], mLawyerId) > 0;
+                return true;
+            } else {
+                Log.i(TAG,"Create new Contact");
+                return _db.insertContact(contacts[0]) > 0;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result)
+        {
+            Log.i(TAG,"Show List Contact Screen");
+            showListContactScreen(result);
+        }
     }
 }
